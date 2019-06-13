d2e441ffafb63c5575b9c64e8754c5833b25c967
Caleb Black <calebmacdonaldblack@gmail.com>
2019-06-14 09:09:17 +1000

    Add mapbox example

--- src/karma_test_refresh_example/core.cljs
+++ src/karma_test_refresh_example/core.cljs
@@ -1,9 +1,20 @@
 (ns karma-test-refresh-example.core
   (:require
-    [reagent.core :as r]))
+    [reagent.core :as r]
+    ["react-map-gl" :default ReactMapGL]))
+
+(defn view []
+  (r/with-let [viewport (r/atom {:width     400
+                                 :height    400
+                                 :latitude  37.7577
+                                 :longitude -122.4376
+                                 :zoom      8})]
+    [:> ReactMapGL (merge @viewport
+                     {:mapboxApiAccessToken "TOKEN_HERE"
+                      :onViewportChange     (fn [vp] (reset! viewport (js->clj vp)))})]))
 
 (defn mount-root []
-  (r/render [:h1 "FOOBAR!"]
+  (r/render [view]
     (.getElementById js/document "app")))
 
 (defn init []
